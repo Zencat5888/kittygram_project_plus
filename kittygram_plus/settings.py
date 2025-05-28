d5bf9d31e5c519c44481849f3c727f628ab7c992
Zencat5888 <s.y.baranov@gmail.com>
2025-05-28 16:01:26 +0300

    Перенос на рабочий компьютер

--- kittygram_plus/settings.py
+++ kittygram_plus/settings.py
@@ -38,8 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'cats.apps.CatsConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,3 +136,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,13 +1,17 @@
-from django.urls import include, path
-
+# kittygram_plus/urls.py
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
+from django.urls import include, path
 
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
+from rest_framework.authtoken import views
 
 router = DefaultRouter()
-router.register('cats', CatViewSet)
+router.register(r'cats', CatViewSet, basename='cat')
+router.register(r'owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet, basename='lightcat')
 
 urlpatterns = [
+    path('api-token-auth/', views.obtain_auth_token),
     path('', include(router.urls)),
 ]
